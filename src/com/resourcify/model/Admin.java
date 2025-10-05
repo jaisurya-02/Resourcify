4714f79f685d5644f8db6fda056eaa6ad39d27b5
Jaisurya <262jaisuryas@gmail.com>
2025-10-05 19:42:17 +0530

    Added Addresource() to Admin.java

--- src/com/resourcify/model/Admin.java
+++ src/com/resourcify/model/Admin.java
@@ -24,6 +24,19 @@ public class Admin extends User {
         return "admin";
     }
     
+    public boolean addResource(String name, String type, boolean availability) {
+        DatabaseManager dbManager = DatabaseManager.getInstance();
+        boolean success = dbManager.addResource(name, type, availability);
+        
+        if (success) {
+            System.out.println("Resource added successfully: " + name);
+        } else {
+            System.out.println("Failed to add resource: " + name);
+        }
+        
+        return success;
+    }
+    
     public boolean removeResource(int resourceId) {
         DatabaseManager dbManager = DatabaseManager.getInstance();
         boolean success = dbManager.removeResource(resourceId);
